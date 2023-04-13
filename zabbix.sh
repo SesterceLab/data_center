871bace39b210cd9a63c7a2b394ce967d933a2cb
hbz <hongbo@sesterce.com>
2023-04-13 12:47:08 +0000

    tested

--- zabbix.sh
+++ zabbix.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
+
 GREEN=$'\e[0;32m'
 RED=$'\e[0;31m'
 NC=$'\e[0m'
-apt purge zabbix-agent
+sudo apt purge zabbix-agent
 wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu20.04_all.deb
-dpkg -i zabbix-release_6.4-1+ubuntu20.04_all.deb
-apt update
-apt install zabbix-agent
+sudo dpkg -i zabbix-release_6.4-1+ubuntu20.04_all.deb
+sudo apt update
+sudo apt -y install zabbix-agent
 rm zabbix-release_6.4-1+ubuntu20.04_all.deb
-service zabbix-agent restart
-# mkdir -p /etc/zabbix/zabbix_agentd.conf.d
-# sh -c "openssl rand -hex 32 > /etc/zabbix/zabbix_agentd.psk"
-# rm -f /etc/zabbix/zabbix_agentd.conf
-# cp ./zabbix_agentd.conf /etc/zabbix/
-# chmod +x ./zxgpu.sh
-# ./zxgpu.sh
-# systemctl status zabbix-agent
-# echo "${GREEN}Remember the following key for zabbix server psk-encryption, psk-indentity gpuserver${NC}"
-# cat /etc/zabbix/zabbix_agentd.psk
-# echo "${GREEN}Finished!${NC}"
+sudo service zabbix-agent restart
+sudo mkdir -p /etc/zabbix/zabbix_agentd.conf.d
+sudo sh -c "openssl rand -hex 32 > /etc/zabbix/zabbix_agentd.psk"
+sudo rm -f /etc/zabbix/zabbix_agentd.conf
+sudo cp ./zabbix_agentd.conf /etc/zabbix/
+chmod +x ./zxgpu.sh
+sudo ./zxgpu.sh
+sudo sevice zabbix-agent restart
+sudo systemctl status zabbix-agent
+echo "${GREEN}Remember the following key for zabbix server psk-encryption, psk-indentity gpuserver${NC}"
+cat /etc/zabbix/zabbix_agentd.psk
+echo "${GREEN}Finished!${NC}"
